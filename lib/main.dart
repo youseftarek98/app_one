49f5730e95ef58c7103599d2759b036fcbe7a00f
unknown <youssef.eng9898@gmail.com>
2022-08-22 02:25:50 +0200

    second

--- lib/main.dart
+++ lib/main.dart
@@ -49,6 +49,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  String name = "hello" ;
 
   void _incrementCounter() {
     setState(() {
